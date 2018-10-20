96158e28e4a69cf86dea63f82a21337520f0a675
Laver0ck <they.all.alike@gmail.com>
2018-10-20 20:10:12 +0300

    Updates

---
+++ precache-manifest.b9bae3126cfb290a700b17c5735ca06c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-app/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "e8623a71870c6fdab243",
+    "url": "/robofriends-app/static/js/runtime~main.e8623a71.js"
+  },
+  {
+    "revision": "ab1a4315b3d8302e66d4",
+    "url": "/robofriends-app/static/js/main.ab1a4315.chunk.js"
+  },
+  {
+    "revision": "e7ae8bb3757ad90b4670",
+    "url": "/robofriends-app/static/js/1.e7ae8bb3.chunk.js"
+  },
+  {
+    "revision": "ab1a4315b3d8302e66d4",
+    "url": "/robofriends-app/static/css/main.6396d420.chunk.css"
+  },
+  {
+    "revision": "e7ae8bb3757ad90b4670",
+    "url": "/robofriends-app/static/css/1.833d4750.chunk.css"
+  },
+  {
+    "revision": "01e11b2f6444531565c324397b629bc6",
+    "url": "/robofriends-app/index.html"
+  }
+];
