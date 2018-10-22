4c07402e2f77e3ed29de7e0e8d9e4d7608813b8a
Laver0ck <they.all.alike@gmail.com>
2018-10-22 11:47:24 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/robofriends-app/precache-manifest.b9bae3126cfb290a700b17c5735ca06c.js"
+  "/robofriends-app/precache-manifest.1802e0ec62c1d526c8cb4c16bcb8bcdb.js"
 );
 
 workbox.clientsClaim();
